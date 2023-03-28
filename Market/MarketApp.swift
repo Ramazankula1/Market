3ae17a64776a4d74df14393f8cec31b4c6821f1f
Ramazan Kula <ramazan.kula@live.de>
2023-03-28 02:28:10 +0200

    Initial Commit

---
+++ Market/MarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarketApp.swift
+//  Market
+//
+//  Created by Ramazan Kula on 28.03.23.
+//
+
+import SwiftUI
+
+@main
+struct MarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
